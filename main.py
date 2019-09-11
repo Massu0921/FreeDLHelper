c6f163c9fb8178bc3ff99b7d08491668e63e071d
Massu <imomushi.pipi@gmail.com>
2019-09-11 18:43:26 +0900

    [change] 一部ウィジェットをインスタンス変数に

--- main.py
+++ main.py
@@ -20,22 +20,22 @@ class MyFrame(wx.Frame):
         root_panel = wx.Panel(self)
 
         # アートワーク
-        aw_panel = ArtworkPanel(root_panel, imgsize=(300, 300))
+        self.aw_panel = ArtworkPanel(root_panel, imgsize=(300, 300))
         # ファイルパス
-        fr_panel = FileRefPanel(root_panel)
+        self.fr_panel = FileRefPanel(root_panel)
         # 曲情報
-        ai_panel = AudioInfoPanel(root_panel)
+        self.ai_panel = AudioInfoPanel(root_panel)
         # URL入力欄
-        url_panel = URLTextPanel(root_panel)
+        self.url_panel = URLTextPanel(root_panel)
         # ボタン
-        bt_panel = ButtonPanel(root_panel)
+        self.bt_panel = ButtonPanel(root_panel)
 
         root_layout = wx.GridBagSizer()
-        root_layout.Add(aw_panel, (0, 0), (2, 1), flag=wx.ALL, border=10)
-        root_layout.Add(fr_panel, (0, 1), (1, 1), flag=wx.EXPAND | wx.ALL, border=10)
-        root_layout.Add(ai_panel, (1, 1), (1, 1), flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
-        root_layout.Add(url_panel, (2, 0), (1, 2), flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
-        root_layout.Add(bt_panel, (3, 0), (1, 2), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
+        root_layout.Add(self.aw_panel, (0, 0), (2, 1), flag=wx.ALL, border=10)
+        root_layout.Add(self.fr_panel, (0, 1), (1, 1),flag=wx.EXPAND | wx.ALL, border=10)
+        root_layout.Add(self.ai_panel, (1, 1), (1, 1), flag=wx.EXPAND |wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
+        root_layout.Add(self.url_panel, (2, 0), (1, 2), flag=wx.EXPAND |wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
+        root_layout.Add(self.bt_panel, (3, 0), (1, 2),flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
         root_layout.AddGrowableCol(1)
 
         root_panel.SetSizer(root_layout)
@@ -47,7 +47,7 @@ class MyFrame(wx.Frame):
 
 class FileRefPanel(wx.Panel):
     """ ファイルパス入力部分 """
-    
+
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -71,7 +71,7 @@ class FileRefPanel(wx.Panel):
         layout = wx.StaticBoxSizer(s_box, wx.HORIZONTAL)
         layout.Add(grid, 1)
         self.SetSizer(layout)
-    
+
     def click_bt_file(self, event):
         """
         ダイアログ表示・パス取得イベント
@@ -85,7 +85,7 @@ class FileRefPanel(wx.Panel):
             filepath = dialog.GetPath()
             # テキストボックスにパス設定
             self.tc_file.SetValue(filepath)
-        
+
         # ダイアログを破棄
         dialog.Destroy()
 
@@ -155,7 +155,7 @@ class AudioInfoPanel(wx.Panel):
         s_box = wx.StaticBox(self, -1, '曲情報')
 
         # 配置
-        grid = wx.FlexGridSizer(cols=2, gap=(0,0))
+        grid = wx.FlexGridSizer(cols=2, gap=(0, 0))
         grid.Add(st_title, flag=wx.ALL, border=10)
         grid.Add(self.tc_title, flag=wx.EXPAND | wx.ALL, border=10)
         grid.Add(st_album, flag=wx.ALL, border=10)
@@ -175,7 +175,14 @@ class AudioInfoPanel(wx.Panel):
 
 
 class URLTextPanel(wx.Panel):
-    """ URL入力パネル """
+    """
+    URL入力パネル
+
+    Attributes
+    ----------
+    tc_url : wx.TextCtrl
+        URLのテキストボックス
+    """
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -183,11 +190,11 @@ class URLTextPanel(wx.Panel):
         s_box = wx.StaticBox(self, -1, 'SoundCloudのURL')
 
         # ** 各項目 **
-        tc_url = wx.TextCtrl(self, -1)
+        self.tc_url = wx.TextCtrl(self, -1)
 
         # 配置
         grid = wx.FlexGridSizer(cols=1, gap=(0, 0))
-        grid.Add(tc_url, flag=wx.EXPAND | wx.ALL, border=10)
+        grid.Add(self.tc_url, flag=wx.EXPAND | wx.ALL, border=10)
 
         # 引き伸ばし
         grid.AddGrowableCol(0)
@@ -202,7 +209,7 @@ class ButtonPanel(wx.Panel):
 
     def __init__(self, parent):
         super().__init__(parent)
-        
+
         # ** 各項目 **
         bt_get = wx.Button(self, -1, label='情報取得')
         bt_edit = wx.Button(self, -1, label='書き込み')
